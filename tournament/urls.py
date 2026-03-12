724097ed74d4782b2f8f5aee378b5114ecf282e5
Ryan Showalter <ryants@fresnovalves.com>
2026-03-12 10:34:14 -0700

    Add a download invoice button on conf page

--- tournament/urls.py
+++ tournament/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('confirmation/<uuid:token>/', views.confirmation, name='confirmation'),
+    path('confirmation/<uuid:token>/invoice/', views.invoice_pdf, name='invoice_pdf'),
     path('donate-raffle-item/', views.raffle_donate, name='raffle_donate'),
     path('donate-raffle-item/thanks/', views.raffle_donate_thanks, name='raffle_donate_thanks'),
 ]
